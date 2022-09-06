5bcdcf71db7cfe3160c96e670fff7aee62851e8e
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-09-06 11:07:18 +0200

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+const autoprefixer = require("autoprefixer");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}", "./index.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
