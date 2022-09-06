49da70002f602bbc3bced5b9d6cbfaa952a0c16f
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-09-06 15:01:43 +0200

    scaffolding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,21 @@ const autoprefixer = require("autoprefixer");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./index.html"],
+	content: ["./src/**/*.{html,js}", "./index.html", "./.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				lightsage: "#78a5a6",
+				darksage: "#3f5e61",
+				lightgrey: "#f1f5f9",
+				darkgreen: "#3a595c",
+				xmasgreen: "#3a595c",
+				sunyellow: "e9bb3d",
+				rainblue: "#6994fa",
+				turtlegreen: "#529873",
+				oceangreen: "#bcd9d8",
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
