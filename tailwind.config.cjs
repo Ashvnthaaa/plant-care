377015a5fe926007b1af03fd47ee000f962635f8
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-09-07 10:52:41 +0200

    add basic structure and layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const autoprefixer = require("autoprefixer");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./index.html", "./.html"],
+	content: ["./src/**/*.{html,js}", "./index.html", "./*.html"],
 	theme: {
 		extend: {
 			backgroundImage: {
