8e3d5ad60c0e890041a0b34b693365a308637d2c
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-09-07 10:20:30 +0200

    add main layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				hero: "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2676')",
+				hero: "url('../public/pilea-hang-unsplash.jpg')",
+				plah: "url('../public/placeholder-150x150.png')",
 			},
 			colors: {
 				lightsage: "#78a5a6",
